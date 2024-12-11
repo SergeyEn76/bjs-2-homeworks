dea920a3b12496c7a100d2b07e06295e2cde4fc6
Sergey En <enyuhin@yandex.ru>
2024-12-11 21:49:38 +0700

    Delette temporary file temp.js and made some corrections

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-  if (d < 0) {
-    arr = [];
-  } else if (d == 0) {
+  if (d == 0) {
     arr.push(-b / (2 * a));
   } else if (d > 0) {
     arr.push((-b + Math.sqrt(d) )/(2*a));
@@ -19,7 +17,6 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let payment = credit * (percentPerMonth + (percentPerMonth / (Math.pow((1 + percentPerMonth), countMonths) - 1)));
   let totalAmount = payment * countMonths;
   totalAmount = (totalAmount).toFixed(2);
-  totalAmount = parseFloat(totalAmount);
-  return totalAmount;
+  return parseFloat(totalAmount);
 }
 
