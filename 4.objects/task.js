cf0959f227997fe3c6ca959defbaeebcbb540d87
Sergey En <enyuhin@yandex.ru>
2024-12-29 23:56:17 +0700

    task 4 - fix some mistake

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -6,50 +6,27 @@ function Student(name, gender, age) {
     this.marks = [];
 }
 
-//let maria = new Student("Maria", "женский", 35);
-//console.log(maria);
-
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
 }
 
-//maria.setSubject("Algebra");
-
 Student.prototype.addMarks = function (...marks) {
     if (this.hasOwnProperty("marks") === false) {
-        //console.log('плохая учеба');
-        return undefined;
+        return;
     }
-    for (let i = 0; i < marks.length; i++) {
-        this.marks.push(marks[i]);
-    }
+    this.marks.push(...marks);
 }
-
-//maria.addMarks();
-//maria.addMarks(4, 5, 4, 5);
 
 Student.prototype.getAverage = function () {
-    if (this.marks.length === 0) {
+    if ((this.hasOwnProperty("marks") === false) || (this.marks.length === 0)) {
         return 0;
     }
-    let countMarks = 0;
-    let sumMarks = 0;
-    for (let i = 0; i < this.marks.length; i++) {
-        sumMarks += this.marks[i];
-        countMarks++;
-    }
-    return sumMarks / countMarks;
+    const sumMarks = this.marks.reduce((acc, item) => acc + item, 0);
+    return sumMarks / this.marks.length;
 }
-
-//console.log(maria.getAverage());
 
 Student.prototype.exclude = function (reason) {
     delete this.subject;
     delete this.marks;
     this.excluded = reason;
 }
-
-//console.log(maria);
-//maria.exclude("плохая учёба");
-//console.log(maria);
-//maria.addMarks(4, 5, 4, 5);
